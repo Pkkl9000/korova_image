804a92c501d2f8f62a826e42a10be2af44e4167c
Pkkl9000 <papryanikov1@gmail.com>
2024-10-31 00:32:05 +0300

    let's go 1

--- src/main/java/dpa/helper/korova_image/AppRunner.java
+++ src/main/java/dpa/helper/korova_image/AppRunner.java
@@ -10,75 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRunner implements ApplicationRunner {
 
-//    private final MyService myService;
-//    SharedList sharedList = new SharedList();
-//    BlockingQueue<Command> queue = new LinkedBlockingQueue<>();
-//
-//    BrowserService browserService;
-
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         new MethodCaller().callMethod("sign");
 
-
-
-//        OpenExample openExample = new OpenExample();
-//        openExample.openPage();
-
-//        browserService.interactWithApp();
-
-
-
-
-
-//        System.out.println("Приложение запущено с аргументами: ");
-//        for (String name : args.getOptionNames()) {
-//            System.out.println(name + ": " + args.getOptionValues(name));
-//        }
-
-//        myService.myMethod("myService started", 42);
-
-//        InputReadRunner.startReading();
-
-
-
-//// +++++++++++++++++
-//
-//        ThreadStartObserver observer = new ThreadStartObserver();
-//        ExampleThread exampleThread = new ExampleThread(observer);
-//
-//        Thread thread = new Thread(exampleThread);
-//        thread.start();
-//
-//        Thread reader = new Thread(new ConsoleReader(sharedList, queue));
-//        Thread dispatcher = new Thread(new ConsoleInputDispatcher(sharedList, queue));
-//
-//        reader.start();
-//        dispatcher.start();
-//
-//        // +++++++++++++++++
-
-//        System.out.println(MethodInspector.getMethodsInfo("dpa.helper.sugg0.action_box"));
-
     }
 }
 
-
-
-
-//@Component
-//public class AppRunner implements CommandLineRunner {
-//
-//    private final MyService myService;
-//
-//    public AppRunner(MyService myService) {
-//        this.myService = myService;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        myService.myMethod("Hello, World!", 42);
-//    }
-//}
