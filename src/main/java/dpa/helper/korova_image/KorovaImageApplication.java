4cfa8b689d57d97cc7653a78cea06805ec637dd9
Pkkl9000 <papryanikov1@gmail.com>
2024-11-20 21:28:41 +0300

    add file-based h2 db

--- src/main/java/dpa/helper/korova_image/KorovaImageApplication.java
+++ src/main/java/dpa/helper/korova_image/KorovaImageApplication.java
@@ -2,7 +2,12 @@ package dpa.helper.korova_image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("dpa.helper.korova_image.h2_demo_layers.domain.repository")
+@EntityScan("dpa.helper.korova_image.h2_demo_layers.domain.entity")
 @SpringBootApplication
 public class KorovaImageApplication {
 
--- src/main/java/dpa/helper/korova_image/db_utils/UserRepository.java
+++ src/main/java/dpa/helper/korova_image/db_utils/UserRepository.java
@@ -2,6 +2,8 @@ package dpa.helper.korova_image.db_utils;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import dpa.helper.korova_image.db_utils.entities.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
